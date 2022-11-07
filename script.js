b6504519c6f883208871700f420820a31b9b2e15
Stefano <sena.stefano@gmail.com>
2022-11-07 12:00:33 -0500

    transfered over and began code regactoring

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Random number variable
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let highscore = 0;
+let score = 20;
+
+// Click Events
+// Select class, add event listener (function expression)
+document.querySelector('.check').addEventListener('click', function () {
+  //ensure guess variable is a number
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //DOM manupulation if no guess
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+    // Player Wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+
+    // wrong guess
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High!' : 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
